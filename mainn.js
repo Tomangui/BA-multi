c34c8850b7d11456a1f6fa9bea86231db9c303da
Tomangui <148499717+Tomangui@users.noreply.github.com>
2023-11-16 20:03:06 -0300

    Add files via upload
    Ultima actualizacion, pagina completada

---
+++ mainn.js
@@ -0,0 +1,10 @@
+function mostrarAlerta() {
+    alert("Club alumni, fundado en 1898 fue el equipo que mas titulos gano en el amateurismo, desaparecio en 1913");}
+
+    function mostrarAlerta_(){
+        alert("Equipo de Boca Juniors, que tiene el logro de ser el plantel que por mas partidos se mantuvo invicto en la era profesional")
+    }
+    function _mostrarAlerta(){
+        alert("Equipo de River plate, primer campeon de la era profesional")
+    }
+
